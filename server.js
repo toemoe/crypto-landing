98e68f86d0b940220bcc9a28f6715f6ea1e68b76
strangka <strangka@student.21-school.ru>
2025-06-08 21:44:31 +0500

    change

--- server.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-
-const app = express()
-const port = 80
-
-app.use(express.static('frontend/dist'))
-
-app.listen(port, () => console.log(`Server running on ${port}`))
